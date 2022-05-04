54f986a21f674f43881d542de79a2dc241a70e6f
Maninho1979 <anderson.s.alves2@gamil.com>
2022-05-04 18:16:41 -0300

    first commit

---
+++ ApiEmpresa.Infra.Data/Repositories/EmpresaRepository.cs
@@ -0,0 +1,58 @@
+﻿using ApiEmpresa.Infra.Data.Contexts;
+using ApiEmpresa.Infra.Data.Entities;
+using ApiEmpresas.Infra.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiEmpresas.Infra.Data.Repositories
+{
+    public class EmpresaRepository : IEmpresaRepository
+    {
+        //atributo
+        private readonly SqlServerContext _context;
+
+        //construtor para injeção de dependência
+        public EmpresaRepository(SqlServerContext context)
+        {
+            _context = context;
+        }
+
+        public void Inserir(Empresa entity)
+        {
+            _context.Entry(entity).State = EntityState.Added;
+            _context.SaveChanges();
+        }
+
+        public void Alterar(Empresa entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Excluir(Empresa entity)
+        {
+            _context.Entry(entity).State = EntityState.Deleted;
+            _context.SaveChanges();
+        }
+
+        public List<Empresa> Consultar()
+        {
+            return _context.Empresa
+                .OrderBy(e => e.NomeFantasia)
+                .ToList();
+        }
+
+        public Empresa ObterPorId(Guid id)
+        {
+            return _context.Empresa
+                .FirstOrDefault(e => e.IdEmpresa.Equals(id));
+        }
+
+        public Empresa ObterPorCnpj(string cnpj)
+        {
+            return _context.Empresa
+                .FirstOrDefault(e => e.Cnpj.Equals(cnpj));
+        }
+    }
+}
+
---
+++ ApiEmpresas.Services/Requests/EmpresaPostRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiEmpresas.Services.Requests
+{     
+    /// <summary>
+/// class pra modelagem de requisição e cadastro de empresa
+/// </summary>
+    public class EmpresaPostRequest
+    {
+        [Required(ErrorMessage = "Informe o nome fantasia.")]
+        public string? NomeFantasia { get; set; }
+
+        [Required(ErrorMessage = "Informe a razão social.")]
+        public string? RazaoSocial { get; set; }
+
+        [Required(ErrorMessage = "Informe o cnpj.")]
+        public string? Cnpj { get; set; }
+    }
+}
